83f7fa5b3e6b50b708e1a22050774fd466b629d0
Sunghwan Park <sunghwpa@c8r3s13.42seoul.kr>
2023-08-24 21:09:51 +0900

    ex02

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunghwpa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 21:00:51 by sunghwpa          #+#    #+#             */
+/*   Updated: 2023/08/24 21:06:23 by sunghwpa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	z;
+
+	z = 'z';
+	while (z >= 'a')
+	{
+		write(1, &z, 1);
+		z--;
+	}
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
