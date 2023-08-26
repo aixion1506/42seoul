f4a739c5ab2a4a623eba35168eab8b5e1ece02df
Sunghwan Park <sunghwpa@c8r5s9.42seoul.kr>
2023-08-26 21:17:35 +0900

    Rush00

---
+++ Rush00/rush04.c
@@ -0,0 +1,94 @@
+extern void ft_putchar(char c);
+
+void    first_line(int x)
+{
+    int w;
+
+    w = 0;
+    while (w < x)
+    {
+        if (w == 0)
+        {
+			ft_putchar('A');
+        }
+        else if (w == x - 1)
+        {
+            ft_putchar('C');
+        }
+        else
+        {
+            ft_putchar('B');
+        }
+        w ++;
+    }
+}
+
+void    last_line(int x)
+{
+    int w;
+
+    w = 0;
+    while (w < x)
+    {
+        if (w == 0)
+        {
+            ft_putchar('C');
+        }
+        else if (w == x - 1)
+        {
+            ft_putchar('A');
+        }
+        else
+        {
+            ft_putchar('B');
+        }
+        w ++;
+    }
+}
+
+void    middle_line(int x)
+{
+    int w;
+
+    w = 0;
+    while (w < x)
+    {
+        if (w == 0)
+        {
+            ft_putchar('B');
+        }
+        else if (w == x - 1)
+        {
+            ft_putchar('B');
+        }
+        else
+        {
+            ft_putchar(' ');
+        }
+    w ++;
+	}
+}
+
+void	rush(int x, int y)
+{
+	int l;
+
+	l = 0;
+	while (l < y)
+	{
+		if (l == 0)
+		{
+			first_line(x);
+		}
+		else if (l == y -1)
+		{
+			last_line(x);
+		}
+		else
+		{
+			middle_line(x);
+		}	
+		l ++;
+		ft_putchar('\n');
+	}
+}
