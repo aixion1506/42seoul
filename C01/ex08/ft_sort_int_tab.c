3a7256ff5a69063d1f673237e708d080d39529ab
Sunghwan Park <sunghwpa@c10r5s2.42seoul.kr>
2023-09-02 17:03:31 +0900

    c01_ex08

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,32 @@
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int i;
+	int tmp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			tmp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = tmp;
+			i = 0;
+		}
+		else
+			i++;
+	}
+}
+
+
+#include <stdio.h>
+
+int	main()
+{
+	int tab[5] = {8, 2, 5, 4, 3};
+	int size = 5;
+
+	ft_sort_int_tab(tab, size);
+	printf("%d, %d, %d, %d, %d", tab[0], tab[1], tab[2], tab[3], tab[4]);
+	return 0;
+}
