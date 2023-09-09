44f79b624f904cffe91b423f52f022071788e619
Sunghwan Park <sunghwpa@c8r5s12.42seoul.kr>
2023-09-09 21:28:05 +0900

    C05_ex02

---
+++ C05/ex02/ft_iterative_power.c
@@ -0,0 +1,23 @@
+int ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	result = nb;
+	if (power  < 0)
+		return 0;
+	if (power == 0)
+		return 1;
+	while (power > 1)
+	{
+		result *= nb;
+		power--;
+	}
+	return (result);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%i", ft_iterative_power(2, 3 ));
+}
