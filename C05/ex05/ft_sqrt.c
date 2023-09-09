07216bcb2b58bf31d670f8a9389befe7fa71465c
Sunghwan Park <sunghwpa@c8r5s12.42seoul.kr>
2023-09-09 21:29:53 +0900

    C05_ex05

---
+++ C05/ex05/ft_sqrt.c
@@ -0,0 +1,32 @@
+int ft_sqrt(int nb)
+{
+	long	index;
+	long	b;
+
+	b = nb;
+	if (b <= 0)
+		return (0);
+	if ( b == 1)
+		return (1);
+	index = 2;
+	if (b >= 2)
+	{
+		while (index * index <= b)
+		{
+			if (index * index == b)
+			{
+				return (index);
+			}
+			index++;
+		}
+	}
+	return (0);
+}
+#include <stdio.h>
+int main() {
+    int number = 25;
+    int result = ft_sqrt(number);
+
+    printf("제곱근: %d\n", result);
+    return 0;
+}
