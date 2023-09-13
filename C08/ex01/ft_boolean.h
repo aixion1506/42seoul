f33bbd6dc968d489726d98109fd42e3c7ebb4e58
Sunghwan Park <sunghwpa@c10r3s11.42seoul.kr>
2023-09-13 22:57:12 +0900

    C08_ex01

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunghwpa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 14:25:48 by sunghwpa          #+#    #+#             */
+/*   Updated: 2023/09/13 14:36:59 by sunghwpa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef enum a_bool
+{
+	false = 0,
+	true = 1,
+}	t_bool;
+
+# define FALSE	false
+# define TRUE	true
+# define EVEN(number) (number % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+#endif
