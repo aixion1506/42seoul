a43fee9af9f6f5113a11bb23dcd9c44bf9d86a5f
Sunghwan Park <sunghwpa@c10r3s11.42seoul.kr>
2023-09-13 22:58:05 +0900

    C08_ex04

---
+++ C08/ex05/ft_show_tab.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunghwpa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 21:53:55 by sunghwpa          #+#    #+#             */
+/*   Updated: 2023/09/13 21:55:36 by sunghwpa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	char	ch;
+	long	lnb;
+
+	lnb = nb;
+	if (lnb < 0)
+	{
+		lnb *= (-1);
+		ft_putchar('-');
+	}
+	ch = (lnb % 10) + '0';
+	if (lnb < 10)
+		ft_putchar(ch);
+	else
+	{
+		lnb /= 10;
+		ft_putnbr(lnb);
+		ft_putchar(ch);
+	}
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str != 0)
+	{
+		ft_putstr(par[i].str);
+		ft_putstr("\n");
+		ft_putnbr(par[i].size);
+		ft_putstr("\n");
+		ft_putstr(par[i].copy);
+		ft_putstr("\n");
+		i++;
+	}
+}
